b281db66d28e8435cfeb7c09a9ca6c96b089f7ee
Matti Paksula <matti.paksula@iki.fi>
2020-02-19 13:24:39 +0200

    silence

--- latest/entrypoint.sh
+++ latest/entrypoint.sh
@@ -1,12 +1,12 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 set -euo pipefail
 export DISPLAY=:0
 
 set +e
-  rm -rf /tmp/*.*
+  rm -rf /tmp/*.* >/dev/null 2>&1
 set -e
 
-Xvfb :0 -screen 0 1366x768x24 &
+Xvfb :0 -screen 0 1366x768x24 >/dev/null 2>&1 &
 
 while true; do
   set +e
@@ -14,20 +14,18 @@ while true; do
     xdpyinfo_code=$?
   set -e
   if [ "$xdpyinfo_code" = "0" ]; then
-    echo "X started"
     break
   fi
-  echo "Waiting X .."
   sleep 0.1
 done
 
-x11vnc -shared -forever -passwd secret &
-fluxbox &
-xeyes &
-chromium-browser --no-sandbox --disable-dev-shm-usage --disable-notifications &
+x11vnc -shared -forever -passwd secret >/dev/null 2>&1 &
+fluxbox >/dev/null 2>&1 &
+xeyes >/dev/null 2>&1 &
+chromium-browser --no-sandbox --disable-dev-shm-usage --disable-notifications >/dev/null 2>&1 &
 
 cd /opt/novnc
-  utils/launch.sh --listen 15900 --vnc localhost:5900 &
+  utils/launch.sh --listen 15900 --vnc localhost:5900 >/dev/null 2>&1 &
 cd /root
 
 exec /root/go/bin/gotty -w -term xterm zsh
